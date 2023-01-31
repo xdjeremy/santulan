b7830b58ec2a40111048631d1523a64be9490b21
jeremy <fran.jeremy@protonmail.com>
2023-02-01 00:26:45 +0800

    fix: add ssr user fetch on articles

--- pages/articles/edit/[id].tsx
+++ pages/articles/edit/[id].tsx
@@ -1,8 +1,21 @@
 import React from "react";
-import { NextPage } from "next";
+import { GetServerSideProps, NextPage } from "next";
 import { EditArticlePage } from "@/components/articles";
+import { useUser } from "@/context";
+import { useEffectOnce } from "usehooks-ts";
+import { pbClient } from "@/utils/ssr";
+import { UsersRoleOptions } from "@/types";
 
-const EditArticle: NextPage = () => {
+interface Props {
+  userData: any;
+}
+const EditArticle: NextPage<Props> = ({ userData }) => {
+  const { setUser } = useUser();
+
+  useEffectOnce(() => {
+    setUser(JSON.parse(userData));
+  });
+
   return (
     <>
       <EditArticlePage />
@@ -10,4 +23,26 @@ const EditArticle: NextPage = () => {
   );
 };
 
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const pb = new pbClient(ctx);
+
+  // get user data
+  const user = pb.getUserData();
+
+  // check if admin
+  if (user.role !== UsersRoleOptions.admin) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      userData: JSON.stringify(user),
+    },
+  };
+};
 export default EditArticle;
--- pages/articles/index.tsx
+++ pages/articles/index.tsx
@@ -1,13 +1,49 @@
 import React from "react";
-import { NextPage } from "next";
+import { GetServerSideProps, NextPage } from "next";
 import { ArticleListPage } from "@/components/articles";
+import { pbClient } from "@/utils/ssr";
+import { UsersRoleOptions } from "@/types";
+import { useUser } from "@/context";
+import { useEffectOnce } from "usehooks-ts";
 
-const Articles: NextPage = () => {
+interface Props {
+  userData: any;
+}
+const Articles: NextPage<Props> = ({ userData }) => {
+  const { setUser } = useUser();
+
+  useEffectOnce(() => {
+    setUser(JSON.parse(userData));
+  });
+
   return (
     <>
       <ArticleListPage />
     </>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const pb = new pbClient(ctx);
+
+  // get user data
+  const user = pb.getUserData();
+
+  // check if admin
+  if (user.role !== UsersRoleOptions.admin) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      userData: JSON.stringify(user),
+    },
+  };
 };
 
 export default Articles;
